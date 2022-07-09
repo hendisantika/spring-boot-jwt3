3c8c06d8cbf95f4ed22922e617fad020cd9f0c1f
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-09 12:28:28 +0700

    #10 - Add Security libs & add BCrypt

--- src/main/java/com/hendisantika/SpringBootJwt3Application.java
+++ src/main/java/com/hendisantika/SpringBootJwt3Application.java
@@ -2,12 +2,19 @@ package com.hendisantika;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringBootJwt3Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJwt3Application.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
     }
 
 }
