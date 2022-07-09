f95979842f1590a6f40c135b1a4c9bf0ef455009
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-09 12:29:40 +0700

    #11 - Add UserService

---
+++ src/main/java/com/hendisantika/service/UserService.java
@@ -0,0 +1,26 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt3
+ * User: powercommerce
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/9/22
+ * Time: 12:28
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepo;
+
+    private final BCryptPasswordEncoder bCryptEncoder;
+
+}
