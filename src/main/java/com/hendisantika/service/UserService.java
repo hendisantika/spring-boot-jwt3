0c50054971d3f6c0d96bf56dc09e652567b6f1e9
Hendi Santika <hendisantika@yahoo.co.id>
2022-07-09 12:30:49 +0700

    #14 - Add loadUserByUsername in UserService

--- src/main/java/com/hendisantika/service/UserService.java
+++ src/main/java/com/hendisantika/service/UserService.java
@@ -3,11 +3,17 @@ package com.hendisantika.service;
 import com.hendisantika.domain.User;
 import com.hendisantika.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 /**
  * Created by IntelliJ IDEA.
@@ -35,5 +41,30 @@ public class UserService implements UserDetailsService {
     //find user by username
     public Optional<User> findByUsername(String username) {
         return userRepo.findByUsername(username);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> opt = userRepo.findByUsername(username);
+
+        org.springframework.security.core.userdetails.User springUser = null;
+
+        if (opt.isEmpty()) {
+            throw new UsernameNotFoundException("User with username: " + username + " not found");
+        } else {
+            User user = opt.get();    //retrieving user from DB
+            Set<String> roles = user.getRoles();
+            Set<GrantedAuthority> ga = new HashSet<>();
+            for (String role : roles) {
+                ga.add(new SimpleGrantedAuthority(role));
+            }
+
+            springUser = new org.springframework.security.core.userdetails.User(
+                    username,
+                    user.getPassword(),
+                    ga);
+        }
+
+        return springUser;
     }
 }
